b34ec113a4592e77754aead3b02164a52d7face4
Yudi Prayoga Raharja <yudiprayogaraharja@gmail.com>
2020-07-12 02:24:02 +0700

    add db migration

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,56 +1,47 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'App/Models/Post'
 
 export default class PostsController {
    
-    private static posts=[
-            { id: 1, title: 'First Post', content: 'This is my first blog post' },
-            { id: 2 , title: 'Second Post', content: 'This is my second blog post'},
-        ]
+    // private static posts=[
+    //         { id: 1, title: 'First Post', content: 'This is my first blog post' },
+    //         { id: 2 , title: 'Second Post', content: 'This is my second blog post'},
+    //     ]
+
     public async index(){
-        return PostsController.posts
+        return await Post.all()
     }
 
     // method save post
     public async store ({ request }: HttpContextContract) {
         const data = request.only(['title', 'content'])
-        const newId = PostsController.posts.length + 1
         const post = {
-            id: newId,
             title: data.title,
             content: data.content,
         }
-        PostsController.posts.push(post)
-        return post
+       return await Post.create(post)
         
     }
 
     // method drop post
     public async drop ({ params }: HttpContextContract) {
-        const postId = Number(params.id) // transform to number
-        PostsController.posts = PostsController.posts.filter(p => p.id !== postId)
+        const post = await Post.find(params.id)
+         post?.delete()
     }
 
     // method to find post
     public async find ({ params }: HttpContextContract) {
-        const postId = Number(params.id)
-        return PostsController.posts.find(p => p.id === postId)
+        return await Post.find(params.id)
     }
 
     // method to update post
     // public async update ({ request,params }: HttpContextContract) {
-    //     const postId = Number(params.id)
     //     const data = request.only(['title', 'content'])
     //     const post = {
-    //         id: postId,
     //         title: data.title,
     //         content: data.content,
     //     }
-
-
-    //     PostsController.posts.find(p => p.id === postId)
-    //     PostsController.posts.replace()
-    //     return post
-
+    //     const post = await Post.updateOrCreate()
     // }
 
 }
